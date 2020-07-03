30abdb79f98044253e8476d6b44544c222d21c13
Michelle David <hilarity.ensues@gmail.com>
2020-07-02 19:29:45 -0700

    adjust layout of result

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
-function add(number1, number2) {
-return number1 + number2;
-}
+const add = function(number1, number2) {
+  return number1 + number2;
+};
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+  const number1 = parseInt($("#add1").val());
+  const number2 = parseInt($("#add2").val());
+  alert(add(number1, number2));
+});
+});
